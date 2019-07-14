7c7b8dfc9338543deb9feac9730dbfee3597fc81
norby97 <simonorbi97@gmail.com>
2019-07-15 02:39:04 +0300

    final

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -12,8 +12,13 @@ export class TabsComponent implements OnInit {
   selectedIndex = 1;
 
   styles = ["alert-info","alert-danger","alert-warning","alert-primary","alert-secondary","alert-light","alert-dark","alert-success"];
+  bgstyles = ["bg-info","bg-danger","bg-warning","bg-primary","bg-secondary","bg-dark","bg-success"];
 
-
+  works = [
+    {id: 1, name:'Internship (4 months)',title:"Consultant/developer", description:"A MES web-application developing with PHP and jQuery.", footer:"Premium World, Satu Mare", style:this.styles[Math.floor(Math.random() * this.styles.length)]},
+    {id: 2, name:'Group Project (1 semester)', title:"Frontend developer", description:"Laravel web-application to calculate WLTP values.", footer:"Babes-Bolyai University, Cluj Napoca", style:this.styles[Math.floor(Math.random() * this.styles.length)]},
+    {id: 3, name:'Work(5 months)', title:"Full Stack Developer", description:"A MES web-application developing with PHP and jQuery.", footer:"Premium World, Satu Mare", style:this.styles[Math.floor(Math.random() * this.styles.length)]}
+  ]
 
   personal = [
       {id: 1, name:'Name: Simon Norbert'},
@@ -25,7 +30,10 @@ export class TabsComponent implements OnInit {
       {id: 2, desc:'Team player and multitasker', style:this.styles[Math.floor(Math.random() * this.styles.length)]},
       {id: 3, desc:'Strive to consistently exceed expectations',style:this.styles[Math.floor(Math.random() * this.styles.length)]},
       {id: 4, desc:'Grasp of multiple technologies and attention to details',style:this.styles[Math.floor(Math.random() * this.styles.length)]},
-      {id: 5, desc:'Logical thinking',style:this.styles[Math.floor(Math.random() * this.styles.length)]}
+      {id: 5, desc:'Logical thinking',style:this.styles[Math.floor(Math.random() * this.styles.length)]},
+      {id: 6, desc:'Awareness of my own limitations',style:this.styles[Math.floor(Math.random() * this.styles.length)]},
+      {id: 7, desc:'Flexible and passionate about programming',style:this.styles[Math.floor(Math.random() * this.styles.length)]},
+      {id: 8, desc:'Patient and creative',style:this.styles[Math.floor(Math.random() * this.styles.length)]}
   ];
 
   languages = [
@@ -48,8 +56,8 @@ export class TabsComponent implements OnInit {
       {id: 1, name:'Maven', exp:'few months experience'},
       {id: 2, name:'Spring Boot', exp:'few months experience'},
       {id: 3, name:'Angular', exp:'1 year experience'},
-      {id: 3, name:'jQuery', exp:'8-9 months experience'},
-      {id: 3, name:'Laravel', exp:'few months experience'}
+      {id: 4, name:'jQuery', exp:'8-9 months experience'},
+      {id: 5, name:'Laravel', exp:'few months experience'}
   ]
 
   constructor(private cdr: ChangeDetectorRef) {
@@ -60,6 +68,10 @@ export class TabsComponent implements OnInit {
 
   setSelected(id: number) {
     this.selectedIndex = id;
+    for(var i=0; i<5; i++)
+      this.abilities[i].style = this.styles[Math.floor(Math.random() * this.styles.length)];
+    for(var i=0; i<3; i++)
+      this.works[i].style = this.bgstyles[Math.floor(Math.random() * this.bgstyles.length)];
   }
 
 }
